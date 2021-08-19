a3b94ce38ed54637bfa63b5cb7c7d48d27be4c32
Juliocrmks <57975456+Juliocrmks@users.noreply.github.com>
2021-08-18 23:34:11 -0500

    Add files via upload

---
+++ Playground01.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var greeting = "Hello, playground"
+var v1 = "esto es swift "
+let c1 = 4
+var cadena:String = String()
+cadena = "Hola mundo"  + v1
+
+print("El contenido es: \(cadena)")
+
+print("El valor de la constante es : \(c1)")
+
+print("El contenido es: \(cadena)")
